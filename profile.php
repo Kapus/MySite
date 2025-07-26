1e491cfddb752ce031172d025ec67f1c0b95eaa0
Marcus <macke_125@hotmail.com>
2025-07-27 01:57:36 +0200

    design bugs

--- profile.php
+++ profile.php
@@ -20,6 +20,11 @@ $score_stmt = $pdo->prepare('SELECT MAX(score) AS top_score FROM flappy WHERE us
 $score_stmt->execute(['user_id' => $user_id]);
 $score_row = $score_stmt->fetch(PDO::FETCH_ASSOC);
 $flappy_top_score = $score_row && $score_row['top_score'] !== null ? $score_row['top_score'] : 0;
+
+// Check if this user has the highest score overall
+$global_stmt = $pdo->query('SELECT user_id, MAX(score) AS top_score FROM flappy GROUP BY user_id ORDER BY top_score DESC LIMIT 1');
+$global_row = $global_stmt->fetch(PDO::FETCH_ASSOC);
+$is_top_user = $global_row && $global_row['user_id'] == $user_id;
 ?>
 <main class="container my-5">
     <div class="row align-items-start">
@@ -31,7 +36,15 @@ $flappy_top_score = $score_row && $score_row['top_score'] !== null ? $score_row[
                         <li class="list-group-item" style="background: transparent;">Username: <?php echo htmlspecialchars($user['username']); ?></li>
                         <li class="list-group-item" style="background: transparent;"><?php echo htmlspecialchars($user['email']); ?></li>
                         <li class="list-group-item" style="background: transparent;">Member since: <?php echo date('Y-m-d', strtotime($user['created_at'])); ?></li>
-                        <li class="list-group-item" style="background: transparent;">Flappy Top Score: <?php echo $flappy_top_score; ?></li>
+                        <li class="list-group-item d-flex align-items-center" style="background: transparent;">Flappy Top Score: <span class="ms-1"><?php echo $flappy_top_score; ?></span>
+                        <?php if ($is_top_user): ?>
+                            <span class="ms-2" style="display:inline-flex; align-items:center; height:1em;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" fill="gold" style="vertical-align:middle;filter:drop-shadow(0 4px 12px #bfa700) drop-shadow(0 0 24px gold);">
+                                    <path d="M3 2a1 1 0 0 0-1 1v2a5 5 0 0 0 4 4.9V14H5a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2h-1V9.9A5 5 0 0 0 14 5V3a1 1 0 0 0-1-1H3zm0 1h10v2a4 4 0 0 1-8 0V3z"/>
+                                </svg>
+                            </span>
+                        <?php endif; ?>
+                        </li>
                     </ul>
                 </div>
             </div>
